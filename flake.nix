b3b3a214a083f849f7286663731ddcac426309fe
Luis-Hebendanz <luis.nixos@gmail.com>
2022-09-01 14:26:09 +0200

    Added chromium

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "NixOS Chrome PWA";
 
   outputs = { self, nixpkgs }: {
-    nixosModule = import ./modules/vscode-server;
+    nixosModule = import ./modules/chrome-pwa;
     nixosModules.default = self.nixosModule;
   };
 }
